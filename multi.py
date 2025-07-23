a4c1a5a1643cc72b6f4473478838e0b3438c3b18
akulaharini12 <hariniakula320@gmail.com>
2025-07-23 12:03:03 +0530

    multi

---
+++ multi.py
@@ -0,0 +1,35 @@
+a=10
+b=20
+c=a+b
+print(c)
+
+#for i in range(65,91):
+  #  print(i)
+
+#for i in range(65,91):
+    #print(chr(i))
+
+from threading import *
+import threading
+
+from threading import *
+
+#t = thread(target=function,name='Alpha',args())
+
+#t.start()
+#t.join()
+
+#def display():
+    #for i in range(65,91):
+       # print(chr(i))
+import threading
+#t=threading.Thread(target=display.name='Alpha',args())
+#t.start()
+#for i in range(65,91):
+   # print(i)
+
+#t.join()
+
+#from threading import Thread
+#class mydata(Thread):
+   # def run(self):
